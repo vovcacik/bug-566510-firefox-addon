a4ad5a7312df131174554a88292251d6b9ae1806
Vlastimil Ovčáčík <vovcacik@github.ovcacik.org>
2013-10-29 14:00:37 +0100

    Change: extending the support up to Firefox 4.0 thru couple minor changes

--- chrome/content/browser-placesOverlay.js
+++ chrome/content/browser-placesOverlay.js
@@ -3,10 +3,10 @@
  */
 var bug566510_PlacesCommandHook = {
     get uniqueSelectedPages() {
-        let uniquePages = {};
-        let URIs = [];
+        var uniquePages = {};
+        var URIs = [];
         gBrowser.selectedTabs.forEach(function (tab) {
-            let spec = tab.linkedBrowser.currentURI.spec;
+            var spec = tab.linkedBrowser.currentURI.spec;
             if (!tab.pinned && !(spec in uniquePages)) {
                 uniquePages[spec] = null;
                 URIs.push(tab.linkedBrowser.currentURI);
@@ -15,7 +15,7 @@ var bug566510_PlacesCommandHook = {
         return URIs;
     },
     bookmarkSelectedPages: function () {
-        let pages = this.uniqueSelectedPages;
+        var pages = this.uniqueSelectedPages;
         if (pages.length > 1) {
             PlacesUIUtils.showBookmarkDialog({ action: "add"
                                              , type: "folder"
