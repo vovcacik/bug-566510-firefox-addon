c6b12882fa34ee111390eca73363792a8830bb16
Vlastimil Ovčáčík <vovcacik@github.ovcacik.org>
2013-10-30 11:59:07 +0100

    Change: onpopupshowing handler now updates multiselectionContextBroadcaster

--- chrome/content/browserOverlay.js
+++ chrome/content/browserOverlay.js
@@ -1,16 +1,15 @@
 var bug566510_TabContextMenu = {
     /**
      * It just hooks on popupshowing event of tab context menu and updates
-     * the 'multiselection' attribute of (all) elements.
+     * the multiselection broadcaster.
      */
     updateContextMenu: function(event) {
         TabContextMenu.updateContextMenu(event);
 
-        var menuItems = document.getElementsByAttribute("multiselection", "*");
         var contextTabs = TabContextMenu.contextTab.multiselected ? gBrowser.selectedTabs : [TabContextMenu.contextTab];
         var multiselection = contextTabs.length > 1;
-        for (var i = 0; i < menuItems.length; i++) {
-            menuItems[i].setAttribute("multiselection", multiselection);
-        }
+
+        var broadcaster = document.getElementById("multiselectionContextBroadcaster");
+        broadcaster.setAttribute("multiselection", multiselection);
     }
 }
